51c98cdccf491936194ca028bb034e7ca164b042
thankzzz <ninoxfirst@gmail.com>
2020-10-20 11:00:44 +0700

    Updates

---
+++ precache-manifest.087bb8f98b52d85eca9eb818e410937f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0fd8bd02898e46b957d8e4c20f2dad",
+    "url": "/testdummy/index.html"
+  },
+  {
+    "revision": "5784eceb03578f70e836",
+    "url": "/testdummy/static/css/main.f9140324.chunk.css"
+  },
+  {
+    "revision": "a746738a32dd45b3261e",
+    "url": "/testdummy/static/js/2.d2a5b48f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testdummy/static/js/2.d2a5b48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5784eceb03578f70e836",
+    "url": "/testdummy/static/js/main.116d6594.chunk.js"
+  },
+  {
+    "revision": "a43158403c46b409dc75",
+    "url": "/testdummy/static/js/runtime-main.06f3de7e.js"
+  }
+]);
